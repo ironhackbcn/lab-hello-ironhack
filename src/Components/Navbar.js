0f3f69f3415c6b0416ccba4fb4ac922151fad6bc
Nessiec86 <nessc86@gmail.com>
2019-04-02 20:26:04 +0200

    done

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="Background">
+                <img src='../images/ironhack-logo.svg' alt="logo" />
+                <img src='../images/menu-top.svg' alt="menu" />
+            </nav>
+        );
+    };
+};
+
+
+export default Navbar;
