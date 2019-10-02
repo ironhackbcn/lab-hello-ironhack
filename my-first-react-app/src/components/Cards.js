ad022776e94c1713a3d9724af7c071979c359638
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-10-02 17:48:22 +0200

    wip

--- my-first-react-app/src/components/Cards.js
+++ my-first-react-app/src/components/Cards.js
@@ -1,18 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "./Card";
 
-class Cards extends  Component  {
-  render() {
-    return (
-      <div className="App-Cards" >
-        <Card name={process.env.PUBLIC_URL + "images/icon1.png"} titleImg="" titleCard="Declarative" comment="React makes it painless to create interactive UIs."/> 
-        <Card name={process.env.PUBLIC_URL + "images/icon2.png"} titleImg="" titleCard="Components" comment="Build encapsulated components that manage theis state."/> 
-        <Card name={process.env.PUBLIC_URL + "images/icon3.png"} titleImg="" titleCard="Single-Ways" comment="A set of inmutable values are passed to the component's."/> 
-        <Card name={process.env.PUBLIC_URL + "images/icon4.png"} titleImg="" titleCard="JSX" comment="Statically-typed designed to run on modern browsers."/> 
-      </div>
-
-    );
-  }
-}
+const Cards = () => {
+  const url = process.env.PUBLIC_URL;
+  const data = [
+    {
+      name: `${url}images/icon1.png`,
+      titleImg: "",
+      titleCard: "Declarative",
+      comment: "React makes it painless to create interactive UIs."
+    },
+    {
+      name: `${url}images/icon2.png`,
+      titleImg: "",
+      titleCard: "Components",
+      comment: "React makes it painless to create interactive UIs."
+    },
+    {
+      name: `${url}images/icon3.png`,
+      titleImg: "",
+      titleCard: "Single-Ways",
+      comment: "A set of inmutable values are passed to the component's,"
+    },
+    {
+      name: `${url}images/icon4.png`,
+      titleImg: "",
+      titleCard: "JSX",
+      comment: "Statically-typed designed to run on modern browsers."
+    }
+  ];
+  return (
+    <div className="App-Cards">
+      data.forEach((info) =>
+      {
+        <Card
+          name={info.name}
+          titleImg={info.titleImg}
+          titleCard={info.titleCard}
+          comment={info.comment}
+        />
+      });
+    </div>
+  );
+};
 
 export default Cards;
